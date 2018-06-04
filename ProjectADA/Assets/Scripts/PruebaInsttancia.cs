1e8974dd226a5f0a5b2a308da341ea1033a0a131
sergiosoftware <sergio.1701311587@ucaldas.edu.co>
2018-06-04 01:32:48 -0500

    Prueba instanciar que sirvio para tres cosas

---
+++ ProjectADA/Assets/Scripts/PruebaInsttancia.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaInsttancia : MonoBehaviour {
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+			GameObject nebulosa= Instantiate(prefab,transform.position,transform.rotation) as GameObject;		
+			
+	}
+}
