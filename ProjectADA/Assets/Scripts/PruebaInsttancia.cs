97e3152af8c53b838124c4f9590bc2dd4e56fe84
sergiosoftware <sergio.1701311587@ucaldas.edu.co>
2018-06-05 00:50:02 -0500

    .

--- ProjectADA/Assets/Scripts/PruebaInsttancia.cs
+++ ProjectADA/Assets/Scripts/PruebaInsttancia.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PruebaInsttancia : MonoBehaviour {
-	public GameObject prefab;
+	private Transform Player;
+	public Rigidbody bola;
+	public float velocidad = 12f;
+
+	//void Disparador(){
+	//	Rigidbody bolaClon = (Rigidbody) Instantiate(bola, transform.position, transform.rotation);
+	//	bolaClon.velocity = transform.forward * velocidad;
+	//}
+
+	//void Awake()
+	//{
+	//	Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	//	
+	//	if (Player == null)
+	//	{
+	//		enabled=false;
+	//		Debug.Log("Variable null");
+	//	}else{
+	//		enabled=true;
+	//		Debug.Log ("La variable no es igual null!");
+	//	}
+	//}
+	//public GameObject prefab;
 
 	// Use this for initialization
-	void Start () {
+	//void Start () {
 		
-	}
+	//}
 	
 	// Update is called once per frame
 	//void Update () {
+	//if (Input.GetKey(KeyCode.Space)){
+	//		Instantiate(bola);
+	//	}
 	//	if (Input.GetKeyUp(KeyCode.Space))
 	//		GameObject nebulosa= Instantiate(prefab,transform.position,transform.rotation) as GameObject;		
 	//		
