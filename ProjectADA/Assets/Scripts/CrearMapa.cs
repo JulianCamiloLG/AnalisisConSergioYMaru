1e9e04c16841100b50617481c38c72ab7459796d
JulianCamiloLG <julycamy_125@hotmail.com>
2018-06-26 10:03:46 -0500

    cambios

--- ProjectADA/Assets/Scripts/CrearMapa.cs
+++ ProjectADA/Assets/Scripts/CrearMapa.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class CrearMapa : MonoBehaviour {
 
 	public GameObject terreno;
+	public GameObject botonTerminar;
+	public GameObject botonPlanetas;
+	public GameObject botonNebulosas;
+	public GameObject botonSimular;
+	public GameObject botonBorrar;
 	
 	public void CrearNebulosas(){
 		terreno.SetActive(true);
+		botonTerminar.SetActive(true);
+
+		botonPlanetas.SetActive(false);
+		botonNebulosas.SetActive(false);
+		botonSimular.SetActive(false);
+		botonBorrar.SetActive(false);
 	}
 
 	public void TerminarNebulosas(){
 		terreno.SetActive(false);
+		botonTerminar.SetActive(false);
+
+		botonPlanetas.SetActive(true);
+		botonNebulosas.SetActive(true);
+		botonSimular.SetActive(true);
+		botonBorrar.SetActive(true);
 	}
 }
