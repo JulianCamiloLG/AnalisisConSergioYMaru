60450fc247305254dad099e3bcc33b7173688867
sergiosoftware <sergio.1701311587@ucaldas.edu.co>
2018-07-14 23:58:39 -0500

    GananciaNebulosa

--- ProjectADA/Assets/Scripts/Simular.cs
+++ ProjectADA/Assets/Scripts/Simular.cs
@@ -33,33 +33,67 @@ public class Simular : MonoBehaviour {
 		}
 		return costos;
 	}
+	int[] materiales = { 10, 20, 30, 40 };
+	int[] planeta = { 1 };
+	int[] sistemaSolar = { 2 };
+	int[][] nebulosa = { sistemaSolar, planeta, materiales };
+	
+	int[] materiales1 = { 150, 250, 350, 450 };
+	int[] planeta1 = { 1 };
+	int[] sistemaSolar1 = { 2 };
+	int[][] nebulosa1 = { sistemaSolar1, planeta1, materiales1 };
+	
+	int[] materiales2 = { 101, 201, 301, 401 };
+	int[] planeta2 = { 1 };
+	int[] sistemaSolar2 = { 2 };
+	int[] materiales3 = { 111, 222, 333, 444 };
+	int[] planeta3 = { 1 };			
+	int[] sistemaSolar3 = { 6 };
+	int[][] nebulosa2 = {
+		sistemaSolar2,
+		planeta2,
+		materiales2,
+		sistemaSolar3,
+		planeta3,
+		materiales3
+	};
+	
+	int[][][] nebulosasTotales = { nebulosa, nebulosa1, nebulosa2 };
+	
+	int[] necesarios = { 20, 50, 68, 10 };
 
-	public static float [] MejorCamino(CrearNebulosa[] nebulosas, float[] totalesNecesarios){
-		float w;
-		float x;
-		float y;
-		float z;
-		float valorSistemaPlanetario;
-		float valorFinalGanancia;
-		var totales = new float[4];
-		if (nebulosas.Length > 1) {
-			for (int i = 0; i < nebulosas.Length; i++) {
-				for (int j = 0; j < nebulosas[i].getSistemasPlanetarios.length; j++) {
-					for (int k = 0; k < nebulosas[i].getSistemasPlanetarios.getPlanetas.length; k++) {
-						for (int l = 0; l < nebulosas[i].getSistemasPlanetarios.getPlanetas.getMateriales.length; l++) {
-							x = nebulosas[i].getSistemasPlanetarios.getPlanetas.getMateriales[l];
-							y = (x * 100) / totalesNecesarios[l];
-							z = z + y;
-						}
-						w = w + z;
+	GananciaNebulosa(nebulosasTotales,necesarios);
+
+	public static float GananciaNebulosa(int[][][] nebulosasTotales, int[] necesarios){
+		int contador = 0, necesario = 0;
+		double w = 0, x = 0, y = 0, z = 0;
+		double valorSistemaPlanetario = 0;
+		foreach (var element in nebulosasTotales) {				
+			foreach (var element1 in element) {
+				foreach (var element2 in element1) {
+					if (contador == 2) {
+						Console.WriteLine("\t" + "Dato" + contador + "->>" + element2.ToString());	
+						x = Int32.Parse(element2.ToString());
+						y = (x * 100) / necesarios[necesario];
+						z = z + y;
+						necesario++;
 					}
-					valorSistemaPlanetario = valorSistemaPlanetario + w;
+				}					
+				contador++;
+				if (contador == 3) {
+					Console.WriteLine("planeta" + z);						
+					w = w + z;
+					Console.WriteLine("Nebulosa" + w);
+					contador = 0;
+					necesario = 0;
+					x = 0;
+					y = 0;
+					z = 0;
+					
 				}
-				valorFinalGanancia = valorFinalGanancia + valorSistemaPlanetario;
-				totales[i] = valorFinalGanancia;
-			}
+			}			
 		}
-		return totales;
+		return w;
 	}
 	
 	
