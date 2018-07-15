66f2f2a7aa0f6e6c904c26d87ea794a465a84bd2
sergiosoftware <sergio.1701311587@ucaldas.edu.co>
2018-07-15 01:09:05 -0500

    Calcular Total de recursos necesario para las mejoras

--- ProjectADA/Assets/Scripts/Simular.cs
+++ ProjectADA/Assets/Scripts/Simular.cs
@@ -21,27 +21,16 @@ public class Simular : MonoBehaviour {
 		botones.SetActive(false);
 	}
 
-	public static int [] MaterialesTotales(int [] mejoras){
-		// disable once SuggestUseVarKeywordEvident
-		var costos = new int[4];
-		if (mejoras.Length>0){
-			for (int i = 0; i < mejoras.Length; i++) {
-				for (int j = 0; j < mejoras.getCosto.length; j++) {
-					costosT[j]=costosT[j]+mejoras.getCosto[j];
-				}
-			}
-		}
-		return costos;
-	}
+	//Calcular los beneficios de una nebulosa
 	int[] materiales = { 10, 20, 30, 40 };
 	int[] planeta = { 1 };
 	int[] sistemaSolar = { 2 };
-	int[][] nebulosa = { sistemaSolar, planeta, materiales };
+	int[][] SistemaPlanetario1 = { sistemaSolar, planeta, materiales };
 	
 	int[] materiales1 = { 150, 250, 350, 450 };
 	int[] planeta1 = { 1 };
 	int[] sistemaSolar1 = { 2 };
-	int[][] nebulosa1 = { sistemaSolar1, planeta1, materiales1 };
+	int[][] SistemaPlanetario2 = { sistemaSolar1, planeta1, materiales1 };
 	
 	int[] materiales2 = { 101, 201, 301, 401 };
 	int[] planeta2 = { 1 };
@@ -49,7 +38,7 @@ public class Simular : MonoBehaviour {
 	int[] materiales3 = { 111, 222, 333, 444 };
 	int[] planeta3 = { 1 };			
 	int[] sistemaSolar3 = { 6 };
-	int[][] nebulosa2 = {
+	int[][] SistemaPlanetario3 = {
 		sistemaSolar2,
 		planeta2,
 		materiales2,
@@ -58,22 +47,21 @@ public class Simular : MonoBehaviour {
 		materiales3
 	};
 	
-	int[][][] nebulosasTotales = { nebulosa, nebulosa1, nebulosa2 };
+	int[][][] Nebulosa = { SistemaPlanetario1, SistemaPlanetario2, SistemaPlanetario3 };
 	
-	int[] necesarios = { 20, 50, 68, 10 };
+	int[] necesarios = { 20, 50, 68, 10 }; //Este funcionara haciendo el llamado a la funcion MaterialesTotales
 
-	GananciaNebulosa(nebulosasTotales,necesarios);
+	GananciaNebulosa(Nebulosa,necesarios);
 
 	public static float GananciaNebulosa(int[][][] nebulosasTotales, int[] necesarios){
 		int contador = 0, necesario = 0;
 		double w = 0, x = 0, y = 0, z = 0;
-		double valorSistemaPlanetario = 0;
-		foreach (var element in nebulosasTotales) {				
-			foreach (var element1 in element) {
-				foreach (var element2 in element1) {
+		foreach (var SistemaPlanetario in Nebulosa) {				
+			foreach (var Planeta in SistemaPlanetario) {
+				foreach (var elemento in Planeta) {
 					if (contador == 2) {
-						Console.WriteLine("\t" + "Dato" + contador + "->>" + element2.ToString());	
-						x = Int32.Parse(element2.ToString());
+						Console.WriteLine("\t" + "Dato" + contador + "->>" + elemento.ToString());	
+						x = Int32.Parse(elemento.ToString());
 						y = (x * 100) / necesarios[necesario];
 						z = z + y;
 						necesario++;
@@ -94,6 +82,32 @@ public class Simular : MonoBehaviour {
 			}			
 		}
 		return w;
+	}
+
+	//Calcular el total de recuros necesarios para las mejoras
+	int[] mejora1 = { 10, 20, 30, 40 };
+	int[] mejora2 = { 100, 200, 300, 400 };
+	int[] mejora3 = { 30, 40, 50, 60 };
+	int[] mejora4 = { 300, 400, 500, 600 };
+	int[] mejora5 = { 50, 60, 70, 80 };
+	int[] mejora6 = { 500, 600, 700, 800 };
+	
+	int[][] Mejoras = { mejora1,mejora2,mejora3,mejora4,mejora5,mejora6 };
+	int [] totales = new int [4];
+	
+	MaterialesTotales(Mejoras,totales);
+
+	public static int [] MaterialesTotales(int [][] MejorasTotales,int [] totales){
+		int contador=0;
+		foreach (var Mejora in MejorasTotales) {
+			contador=0;				
+			foreach (var valorMejora in Mejora) {
+				Console.WriteLine("\t" + "Dato" + valorMejora.ToString());
+				totales[contador]=totales[contador]+Int32.Parse(valorMejora.ToString());
+				contador++;					
+			}				
+		}
+		return totales;			
 	}
 	
 	
